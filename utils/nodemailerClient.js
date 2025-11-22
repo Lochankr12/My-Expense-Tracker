e9eb0d9f8f03ac15a1c56060733c82c6e36b2d1f
unknown <lochankr12@gmail.com>
2025-11-22 14:30:36 +0530

    Use smtp.gmail.com:587 for nodemailer

--- utils/nodemailerClient.js
+++ utils/nodemailerClient.js
@@ -1,11 +1,17 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,          // use STARTTLS on 587
+  secure: false,      // false for 587, true is for 465
   auth: {
-    user: process.env.GMAIL_USER,      // your gmail or Google workspace email
-    pass: process.env.GMAIL_PASS,      // your app password or email password
+    user: process.env.GMAIL_USER, // your Gmail
+    pass: process.env.GMAIL_PASS, // Gmail APP password
   },
+  // Optional but can help with timeouts
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 const sendEmail = async ({ to, subject, html }) => {
@@ -16,7 +22,13 @@ const sendEmail = async ({ to, subject, html }) => {
     html,
   };
 
-  await transporter.sendMail(mailOptions);
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Reset email sent to:', to);
+  } catch (err) {
+    console.error('Reset email error:', err);
+    throw err; // let your controller send 500 to frontend
+  }
 };
 
 module.exports = sendEmail;
